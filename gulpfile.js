96170b858fd2c66e30d64b309a47b1c71e4a1a63
tiffanypaulino <56327590+tiffanypaulino@users.noreply.github.com>
2020-03-15 23:07:15 -0700

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var {src, dest, watch} = require('gulp');
+var sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+
+function css () {
+    return src('src/*.scss')
+        .pipe(sass())
+        .pipe(des('dist/css'))
+};
+
+exports.default = function () {
+    watch('src/*.scss', css);
+    watch('src/modules/*.scss', css);
+};
+
+
+// const gulp = require('gulp');
+// const browserSync = require('browser-sync');
+// const sass = require('gulp-sass');
+
+// // compile sass
+// gulp.task('sass', function(){
+//     return gulp.src(['src/scss/*.scss'])
+//     .pipe(sass())
+//     .pipe(gulp.dest('dist/css'))
+//     .pipe(browserSync.stream());
+// });
+
+// // watch and serve
+// gulp.task('serve', ['sass'], function(){
+//     browserSync.init ({
+//         server: './src'
+//     });
+
+//     gulp.watch(['src/scss/*.scss'], ['sass']);
+//     gulp.watch(['src/*.html']).on('change', browserSync.reload);
+// });
